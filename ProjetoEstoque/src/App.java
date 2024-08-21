0c351d46d065eb1ffa00df8a0bbbe688538bdec7
Matheus Araujo <matheuslp.daraujo@gmail.com>
2024-08-21 18:21:00 -0400

    nenhuma alteração feita pelo vscode.

--- ProjetoEstoque/src/App.java
+++ ProjetoEstoque/src/App.java
@@ -2,7 +2,7 @@ import visao.ClasseProdutoVisao;
 
 public class App {
     public static void main(String[] args) throws Exception {
-            ClasseProdutoVisao visao = new ClasseProdutoVisao();
-            visao.Exibir();
+        ClasseProdutoVisao visao = new ClasseProdutoVisao();
+        visao.Exibir();
     }
 }
