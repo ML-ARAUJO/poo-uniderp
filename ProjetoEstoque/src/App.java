47057fdf8a7499a6a8620f3a6c26d3c3677ec412
Matheus Araujo <matheuslp.daraujo@gmail.com>
2024-09-11 11:07:50 -0400

    testando o objeto criar no App.java.

--- ProjetoEstoque/src/App.java
+++ ProjetoEstoque/src/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         ClasseProdutoVisao visao = new ClasseProdutoVisao();
         visao.Exibir();
+        visao.Criar();
 
     }
 }
