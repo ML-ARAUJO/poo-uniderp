4415051d1e0155b882d4807e4855fa7cbdb22080
Matheus Araujo <matheuslp.daraujo@gmail.com>
2024-09-24 12:05:43 -0400

    update cod git professor

--- ProjetoEstoque/src/visao/ClasseProdutoVisao.java
+++ ProjetoEstoque/src/visao/ClasseProdutoVisao.java
@@ -14,26 +14,70 @@ public class ClasseProdutoVisao {
     this.srv = new ClasseProdutoServico();
   }
 
+  // -----------------------------------------------------------------------------------
   public void Exibir() {
     ArrayList<ClasseProduto> lista = this.srv.Navegar();
     for (ClasseProduto cp : lista) {
       this.Imprimir(cp);
     }
-
   }
 
-  private void Imprimir(ClasseProduto alvo) {
-    System.out.println("Produtos");
-    System.out.println("Código da Classe: " + alvo.getCodigo());
-    System.out.println("Descrição: " + alvo.getDescricao());
-    System.out.println("Data de Inclusão: " + alvo.getDataDeInclusao());
+  public void ExibirPorLinha() {
+    ArrayList<ClasseProduto> lista = this.srv.Navegar();
+    for (ClasseProduto cp : lista) {
+      this.ImprimirPorLinha(cp);
+    }
+  }
+
+  // -----------------------------------------------------------------------------------
+  public void ImprimirPorLinha(ClasseProduto cp) {
+    String mensagem = "";
+    mensagem += "Código: " + cp.getCodigo() + " | ";
+    mensagem += "Descrição: " + cp.getDescricao() + " | ";
+    mensagem += "Data de Inclusão: " + cp.getDataDeInclusao();
+    System.out.println(mensagem);
     System.out.println("---------------------------------------------");
   }
 
-  public void Criar() {
-    ClasseProduto novo = new ClasseProduto(0, "Teste", LocalDate.now());
+  private void Imprimir(ClasseProduto cp) {
+    System.out.println("Produtos");
+    System.out.println("Código da Classe: " + cp.getCodigo());
+    System.out.println("Descrição: " + cp.getDescricao());
+    System.out.println("Data de Inclusão: " + cp.getDataDeInclusao());
+    System.out.println("---------------------------------------------");
+  }
+
+  public void ImprimirPorLinha(int chave) {
+    ClasseProduto cp = this.srv.Ler(chave);
+    this.ImprimirPorLinha(cp);
+
+  }
+
+  // -------------------------------------------------------------------------------------
+  public void Criar(ClasseProduto novo) {
     this.srv.Adicionar(novo);
-    this.Imprimir(novo);
+  }
+
+  // --------------------------------------------------------------------------------------
+  public void Editar(int chave, ClasseProduto alt) {
+    ClasseProduto cp = this.srv.Ler(chave);
+    if (cp != null) {
+      cp.setDescricao(alt.getDescricao());
+
+    } else {
+      System.out.println("Item não Localizado");
+    }
+
+  }
+
+  // ---------------------------------------------------------------------------------------
+  public void Remover(int chave) {
+    ClasseProduto cp = this.srv.Ler(chave);
+    if (cp != null) {
+      this.Remover(chave);
+    } else {
+      System.out.println("Item não encontrado");
+    }
   }
 
 }
