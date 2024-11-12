97df45d8574e48e1e6c0ce5368b48d5291713fe2
Matheus Araujo <matheuslp.daraujo@gmail.com>
2024-11-12 09:21:32 -0400

    Ajustes de remoção import

--- AtividadeA2/src/App.java
+++ AtividadeA2/src/App.java
@@ -1,13 +1,7 @@
-
-//import visao.ClasseFornecedorMenu;
-//import visao.SubclasseFornecedorMenu;
-
 import visao.FornecedorMenu;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // ClasseFornecedorMenu tela = new ClasseFornecedorMenu();
-        // SubclasseFornecedorMenu tela = new SubclasseFornecedorMenu();
         FornecedorMenu tela = new FornecedorMenu();
         tela.ExibirMenu();
     }
